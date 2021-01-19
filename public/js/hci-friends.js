947882fc4ea5b11e9ef62e2d23bd89403777eb99
wangangela2000 <wangangela2000@gmail.com>
2021-01-18 16:25:09 -0800

    update

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+	$(".friend a h3").click(onClick);
 }
 
 function anagrammedName(name) {
@@ -43,4 +44,10 @@ function anagrammedName(name) {
 		console.log(name + " not known for anagramming.");
 		return name;
 	}
+}
+
+function onClick(e) {
+	e.preventDefault();
+	var name = $(this).text();
+	$(this).text(anagrammedName(name));
 }
--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,10 @@
 var data = require("../data.json");
 
 exports.addFriend = function(request, response) {    
-	// Your code goes here
+	var name = request.query.name;
+    var description = request.query.description;
+    var imgUrl = "http://lorempixel.com/400/400/people";
+    var input = {"name": name, "description": description, "imageURL": imgUrl};
+    data.friends.push(input);
+    response.render('index', data);
  }
